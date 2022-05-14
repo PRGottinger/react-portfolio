9dd6b78e57548fc37d632a4fe3c4a829616ed710
Paul Gottinger <pgottinger@outlook.com>
2022-05-14 15:57:07 -0500

    react state/nav bar/pages

---
+++ react-portfolio/src/components/Contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Contact(props) {
+    return (
+        <div>
+            <h1>This is the contact form</h1>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ react-portfolio/src/components/Nav/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import coverImage from "../../assets/keyboard.jpg";
+
+function Nav(props) {
+  const categories = [
+    {
+      name: "Projects",
+      description:
+        "Links to my Github Repositories containing my Development projects",
+    },
+    { name: "Resume", description: "My Resume" },
+  ];
+
+  function categorySelected(name) {
+    console.log(`${name} clicked`);
+  }
+
+  return (
+    <header>
+      <h2>
+        <a href="/">💻 Paul's Web Development Portfolio!</a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li
+            onClick={() => {
+              props.setPage("About");
+            }}
+            className="mx-2"
+          >
+            <a href="#about">About me</a>
+          </li>
+          <li
+            onClick={() => {
+              props.setPage("Contact");
+            }}
+          >
+            <span>Contact</span>
+          </li>
+          <li
+            onClick={() => {
+              props.setPage("Projects");
+            }}
+          >
+            <span>Projects</span>
+          </li>
+
+          <li
+            onClick={() => {
+              props.setPage("Resume");
+            }}
+          >
+            <span>Resume</span>
+          </li>
+          {/* { {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span> }
+            { </li>
+          ))} */}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
